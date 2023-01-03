8dcfc313a9c31ac6f08014a8c4940a595d2000b6
Mykhailo Den <95014048+MikleDen@users.noreply.github.com>
2023-01-03 17:25:29 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+
+function onChangeColor(e){
+    timerId = setInterval(() => {
+        body.style.backgroundColor= getRandomHexColor;
+        
+      }, 1000);
+
+}
+function onStopChangeColor(e){
+    clearInterval(timerId);
+
+}
+btnStart.addEventListener("click", onChangeColor);
+btnStop.addEventListener("click", onStopChangeColor);
