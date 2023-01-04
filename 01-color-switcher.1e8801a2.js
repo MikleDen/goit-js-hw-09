b0a8dc40961b4eab57d6f81e5a77b690c9483f71
MikleDen <95014048+MikleDen@users.noreply.github.com>
2023-01-04 05:18:55 +0000

    Deploying to gh-pages from @ MikleDen/goit-js-hw-09@d554dc31a710b9b2e45ba39655b4ef06312fb864 🚀

---
+++ 01-color-switcher.1e8801a2.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(function(r){e.disabled=!0,t.disabled=!1,timerId=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(function(d){e.disabled=!1,t.disabled=!0,clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.1e8801a2.js.map
